40f2e83afeddc5aac1eb543f0af27129273f27f7
Pha Nguyen <sephanguyen@gmail.com>
2018-04-25 21:26:38 +0700

    typescript basic

--- app.ts
+++ app.ts
@@ -1,7 +1,56 @@
-class HelloWorld {
-    constructor(public message : string){}
+function GetAllBooks() {
+    
+    let books = [
+        {title: 'Ulysees', author: 'James Joyce', available: true, category: Category.Fiction},
+        {title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: true, category: Category.Fiction},
+        {title: 'I Know Why The Caged Brid Signs', author: 'Maya Angelou', available: true, category : Category.Poetry},
+    ]
+    return books;
 }
 
-var hello = new HelloWorld("Hello TypeScript");
-console.log(hello.message);
+function LogFirstAvailable(books) : void{
 
+    let numberOfBooks : number = books.lenght;
+    let firstAvailable : string = '';
+
+    for (const currentBook of books) {
+        
+        if(currentBook.available) {
+            firstAvailable = currentBook.title;
+            break;
+        }
+    }
+    console.log('Total Books: ' + numberOfBooks);
+    console.log('First Available: ' + firstAvailable);
+}
+
+enum Category { Biography, Poetry, Fiction, History, Children}
+
+function GetBookTitlesByCategory(categoryFilter : Category) : Array<string> {
+
+    console.log('Getting books in category: ' + Category[categoryFilter]);
+
+    const allBooks = GetAllBooks();
+    const filteredTitles : string[] = [];
+
+    for (const currentBook of allBooks) {
+        if(currentBook.category === categoryFilter) {
+            filteredTitles.push(currentBook.title);
+        }
+    }
+
+    return filteredTitles;
+}
+
+function LogBookTitles(titles : string[]) : void {
+
+    for (const title of titles) {
+        console.log(title);
+    }
+}
+
+// const allBooks = GetAllBooks();
+// LogFirstAvailable(allBooks);
+
+const peotryBooks = GetBookTitlesByCategory(Category.Poetry);
+LogBookTitles(peotryBooks);
--- js/app.js
+++ js/app.js
@@ -1,9 +1,52 @@
-var HelloWorld = /** @class */ (function () {
-    function HelloWorld(message) {
-        this.message = message;
+function GetAllBooks() {
+    var books = [
+        { title: 'Ulysees', author: 'James Joyce', available: true, category: Category.Fiction },
+        { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: true, category: Category.Fiction },
+        { title: 'I Know Why The Caged Brid Signs', author: 'Maya Angelou', available: true, category: Category.Poetry },
+    ];
+    return books;
+}
+function LogFirstAvailable(books) {
+    var numberOfBooks = books.lenght;
+    var firstAvailable = '';
+    for (var _i = 0, books_1 = books; _i < books_1.length; _i++) {
+        var currentBook = books_1[_i];
+        if (currentBook.available) {
+            firstAvailable = currentBook.title;
+            break;
+        }
     }
-    return HelloWorld;
-}());
-var hello = new HelloWorld("Hello TypeScript");
-console.log(hello.message);
+    console.log('Total Books: ' + numberOfBooks);
+    console.log('First Available: ' + firstAvailable);
+}
+var Category;
+(function (Category) {
+    Category[Category["Biography"] = 0] = "Biography";
+    Category[Category["Poetry"] = 1] = "Poetry";
+    Category[Category["Fiction"] = 2] = "Fiction";
+    Category[Category["History"] = 3] = "History";
+    Category[Category["Children"] = 4] = "Children";
+})(Category || (Category = {}));
+function GetBookTitlesByCategory(categoryFilter) {
+    console.log('Getting books in category: ' + Category[categoryFilter]);
+    var allBooks = GetAllBooks();
+    var filteredTitles = [];
+    for (var _i = 0, allBooks_1 = allBooks; _i < allBooks_1.length; _i++) {
+        var currentBook = allBooks_1[_i];
+        if (currentBook.category === categoryFilter) {
+            filteredTitles.push(currentBook.title);
+        }
+    }
+    return filteredTitles;
+}
+function LogBookTitles(titles) {
+    for (var _i = 0, titles_1 = titles; _i < titles_1.length; _i++) {
+        var title = titles_1[_i];
+        console.log(title);
+    }
+}
+// const allBooks = GetAllBooks();
+// LogFirstAvailable(allBooks);
+var peotryBooks = GetBookTitlesByCategory(Category.Poetry);
+LogBookTitles(peotryBooks);
 //# sourceMappingURL=app.js.map
